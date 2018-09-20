b3bc6d5ef1cff3a37ebe1ab8eac1c0b2df30950d
AlohaJarren <calizo.jarren@gmail.com>
2018-09-19 14:25:05 -1000

    Songs 1/3

--- data.js
+++ data.js
@@ -1,4 +1,113 @@
 const data = [
+    
+    //1
+    {
+        artist: "Kolohe Kai",
+        genre: "Reggaiian",
+        song: "Heartstrings",
+        image: "https://tinyurl.com/es6playlist1",
+        //https://www.youtube.com/watch?v=kgauOG7-3qM
+    },
+
+    //2
+    {
+        artist: "Common Kings",
+        genre: "Reggaiian",
+        song: "Just Another Lover",
+        image: "https://tinyurl.com/es6playlist2",
+        //https://www.youtube.com/watch?v=65ueMdSTh4M
+    },
+
+    //3
+    {
+        artist: "Awa",
+        genre: "Reggaiian",
+        song: "Perfect Day",
+        image: "https://tinyurl.com/es6playlist3",
+        //https://www.youtube.com/watch?v=QAYZmR4REcY
+    },
+
+    //4
+    {
+        artist: "Bruno Mars",
+        genre: "Reggaiian",
+        song: "Liquor Store Blues",
+        image: "https://tinyurl.com/es6playlist4",
+        //https://www.youtube.com/watch?v=1iBm60uJXvs
+    },
+
+    //5
+    {
+        artist: "J Boog",
+        genre: "Reggaiian",
+        song: "Let's Do It Again",
+        image: "https://tinyurl.com/es6playlist5",
+        //https://www.youtube.com/watch?v=q6fgVVlPl08
+    },
+
+    //6
+    {
+        artist: "Israel Kamakawiwoʻole",
+        genre: "Reggaiian",
+        song: "Somewhere Over the Rainbow",
+        image: "https://tinyurl.com/es6playlist6",
+        //https://www.youtube.com/watch?v=V1bFr2SWP1I
+    },
+
+    //7
+    {
+        artist: "Conor Maynard",
+        genre: "Pop YT",
+        song: "All My Love",
+        image: "https://tinyurl.com/es6playlist7",
+        //https://www.youtube.com/watch?v=F9mQtqNAqms
+    },
+
+    //8
+    {
+        artist: "Conor Maynard",
+        genre: "Pop YT",
+        song: "Glass Girl",
+        image: "https://tinyurl.com/es6playlist8",
+        //https://www.youtube.com/watch?v=eRHtgF3pmpQ
+    },
+
+    //9
+    {
+        artist: "Alex Aiono",
+        genre: "Pop YT",
+        song: "Question",
+        image: "https://tinyurl.com/es6playlist9",
+    },
+
+    //10
+    {
+        artist: "Alex Aiono",
+        genre: "Pop YT",
+        song: "One Dance/Hasta el Amanecer Mashup",
+        image: "https://tinyurl.com/es6playlist10",
+        //https://www.youtube.com/watch?v=Kuz3DUNZaC8
+    },
+
+    //11
+    {
+        artist: "William Singe",
+        genre: "Pop YT",
+        song: "Rush",
+        image: "https://tinyurl.com/es6playlist11",
+        //https://www.youtube.com/watch?v=vxPcTpXGOug
+    },
+
+    //12
+    {
+        artist: "William Singe",
+        genre: "Pop YT",
+        song: "Lets Get Married",
+        image: "https://tinyurl.com/es6playlist12",
+        //https://www.youtube.com/watch?v=PjOj2zrpNAE
+    },
+
+    //13
     {
         artist: "",
         genre: "",
@@ -6,5 +115,188 @@ const data = [
         image: "",
     },
 
-    
+    //14
+    {
+        artist: "",
+        genre: "",
+        song: "",
+        image: "",
+    },
+
+    //15
+    {
+        artist: "",
+        genre: "",
+        song: "",
+        image: "",
+    },
+
+    //16
+    {
+        artist: "",
+        genre: "",
+        song: "",
+        image: "",
+    },
+
+    //17
+    {
+        artist: "",
+        genre: "",
+        song: "",
+        image: "",
+    },
+
+    //18
+    {
+        artist: "",
+        genre: "",
+        song: "",
+        image: "",
+    },
+
+    //19
+    {
+        artist: "",
+        genre: "",
+        song: "",
+        image: "",
+    },
+
+    //20
+    {
+        artist: "",
+        genre: "",
+        song: "",
+        image: "",
+    },
+
+    //21
+    {
+        artist: "",
+        genre: "",
+        song: "",
+        image: "",
+    },
+
+    //22
+    {
+        artist: "",
+        genre: "",
+        song: "",
+        image: "",
+    },
+
+    //23
+    {
+        artist: "",
+        genre: "",
+        song: "",
+        image: "",
+    },
+
+    //24
+    {
+        artist: "",
+        genre: "",
+        song: "",
+        image: "",
+    },
+
+    //25
+    {
+        artist: "",
+        genre: "",
+        song: "",
+        image: "",
+    },
+
+    //26
+    {
+        artist: "",
+        genre: "",
+        song: "",
+        image: "",
+    },
+
+    //27
+    {
+        artist: "",
+        genre: "",
+        song: "",
+        image: "",
+    },
+
+    //28
+    {
+        artist: "",
+        genre: "",
+        song: "",
+        image: "",
+    },
+
+    //29
+    {
+        artist: "",
+        genre: "",
+        song: "",
+        image: "",
+    },
+
+    //30
+    {
+        artist: "",
+        genre: "",
+        song: "",
+        image: "",
+    },
+
+    //31
+    {
+        artist: "",
+        genre: "",
+        song: "",
+        image: "",
+    },
+
+    //32
+    {
+        artist: "",
+        genre: "",
+        song: "",
+        image: "",
+    },
+
+    //33
+    {
+        artist: "",
+        genre: "",
+        song: "",
+        image: "",
+    },
+
+    //34
+    {
+        artist: "",
+        genre: "",
+        song: "",
+        image: "",
+    },
+
+    //35
+    {
+        artist: "",
+        genre: "",
+        song: "",
+        image: "",
+    },
+
+    //36
+    {
+        artist: "",
+        genre: "",
+        song: "",
+        image: "",
+    },
+
 ]
