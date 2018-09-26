62fae93ad22b19933725d5bbff51b88cf190aac5
Jarren Calizo <30964113+AlohaJarren@users.noreply.github.com>
2018-09-26 09:00:44 -1000

    Just need to add the music

--- app.js
+++ app.js
@@ -5,11 +5,14 @@ const currentPic = document.querySelector('#currentPic');
 const currentTitle = document.querySelector('#currentName');
 const currentFile = document.querySelector('#currentPlaying')
 
+var musicArray = [];
+
 const dataHTML = data.map((values) => {
     return `
         <img src='${values.image}' />
         <h2> ${values.song} </h2>
         <h3> ${values.artist} </h3>
+        <h3 id='genre'>${values.genre}</h3>
         <!--<p> ${values.file} </p>-->
     `
 });
@@ -22,8 +25,24 @@ for(var i=0; i < dataHTML.length; i++) {
     musicElement.addEventListener("click",() => {
         currentPic.src = musicElement.getElementsByTagName('img')[0].src;
         currentTitle.innerHTML = musicElement.getElementsByTagName('h2')[0].innerHTML;
-        currentFile.innerHTML = musicElement.getElementsByTagName('p')[0].innerHTML;
+        //currentFile.innerHTML = musicElement.getElementsByTagName('p')[0].innerHTML; //comment this out
+        //currentFile.play(); //this too
     });
 
+    musicArray.push(musicElement);
     mainContainer.appendChild(musicElement);
+}
+
+var Categorized = (category) => {
+    if(category.toLowerCase() == 'all') {
+        musicArray.forEach(cur => cur.style.display = 'inline-block');
+    } else {
+        musicArray.filter(cur => {
+            if(category.toLowerCase() === cur.childNodes[7].innerHTML.toLowerCase()) {
+                return cur.style.display = 'inline-block'
+            } else {
+                return cur.style.display = 'none'
+            }
+        });
+    }
 }
